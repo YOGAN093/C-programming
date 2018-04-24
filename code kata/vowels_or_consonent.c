b93f90685e9a5a1545671d70f8705ff0861ce566
YOGAN093 <38650951+YOGAN093@users.noreply.github.com>
2018-04-24 14:46:12 +0530

    Update vowels_or_consonent.c

--- code kata/vowels_or_consonent.c
+++ code kata/vowels_or_consonent.c
@@ -2,7 +2,7 @@
 int main()
 {
 char ch;
-printf("Enter the Character: ");
+printf(" ");
 scanf("%c",&ch);
 if(ch=='A'||ch=='a'||ch=='E'||ch=='e'||ch=='I'||ch=='i'||ch
 =='O'||ch=='o'||ch=='U'||ch=='u')
