b124fc6eb2243fd776c520c94420429968055f33
YOGAN093 <38650951+YOGAN093@users.noreply.github.com>
2018-07-07 12:53:28 +0530

    Create even.c

---
+++ code kata/even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0,i;
+    scanf("%d%d",&a,&b);
+if(a,b<=100000)
+{
+    for(i=a;i<b-1;i++)
+    {
+    c=i+1;
+    if(c%2==0)
+    {
+        printf("%d  ",c);
+    }
+    }
+}
+}
